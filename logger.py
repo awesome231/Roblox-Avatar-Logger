e2579960b86efc0e42e987d14dc9254a3ff9505f
awesome231 <113051068+awesome231@users.noreply.github.com>
2024-11-05 16:13:39 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,12 +1,12 @@
 import requests
 import time
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
-user_id = #set target's userid here
-timeperrequest = 5 # edit time per request
-Webhook = "" # DO NOT SHARE, enter your webhook
+user_id = # enter userid of the user your targetting.
+timeperrequest = 5 # edit inverval where it checks (2 requests)
+Webhook = "" # DO NOT SHARE, enter webnhook url here
 
-cookie = "" # DO NOT SHARE, your FULL roblox cookie, not just ROBLOSECURITY!!
+cookie = "" # DO NOT SHARE, enter FULL roblox cookie here NOT JUST ROBLOXSECURITY
 
 checkwearing = 0
 outfitchanged = 0
@@ -14,7 +14,7 @@ getuser = 0
 first = 0
 oldwear = []
 
-
+embed = DiscordEmbed()
 
 headers = {
     'cookie': cookie
@@ -22,6 +22,7 @@ headers = {
 
 while True:
     data = requests.get(f"https://avatar.roblox.com/v1/users/{user_id}/currently-wearing", headers=headers)
+    headshot = requests.get(f"https://thumbnails.roblox.com/v1/users/avatar?userIds={user_id}&size=420x420&format=Png&isCircular=false")
     if getuser == 0:
         user = requests.get(f"https://users.roblox.com/v1/users/{user_id}", headers=headers)
         user = user.json()
@@ -29,23 +30,31 @@ while True:
     status1 = data.status_code
     print(f"Status Code: {status1}")
     data = data.json()
+    headshot = headshot.json()
+    headshoturl = headshot.get("data", {})
+    headshoturl2 = headshoturl[0].get("imageUrl")
+    if headshoturl2:
+        embed.set_image(url=headshoturl2)
+    else:
+        embed.set_image(url='https://static.wikia.nocookie.net/roblox/images/6/66/Content_Deleted.png/revision/latest/scale-to-width-down/250?cb=20230428000418')
 
     active = data.get("assetIds", [])
     length = len(active)
     print(f"amount of assets wearing: {length}")
     if first == 0 and active != []:
+        oldwear = active
         webhook = DiscordWebhook(url=Webhook, content=f"Targetting **{name}** :dart:\nTheir current avatar consists of {active}")
+        webhook.add_embed(embed)
         response = webhook.execute()
         first = 1
     elif first == 0 and active == []:
         webhook = DiscordWebhook(url=Webhook, content=f"Targetting **{name}**:dart:\nTheir current avatar is **Blank!**")
+        oldwear = active
+        webhook.add_embed(embed)
         response = webhook.execute()
         first = 1
     if checkwearing == 0:
-        oldwear = active
-        print("defaulting to new outfit")
         checkwearing = 1
-        time.sleep(timeperrequest)
     elif checkwearing == 1:
         if oldwear != active:
             print(f"{name} has changed their outfit: ids = {active}")
@@ -53,21 +62,32 @@ while True:
             lossed = set(oldwear) - set(active)
             if active == []:
                 webhook = DiscordWebhook(url=Webhook, content=f"**{name}** has changed their outfit! \nbut they set it so a **Blank Avatar** \n**Removed**: {lossed}")
+                webhook.add_embed(embed)
                 response = webhook.execute()
+                oldwear = active
+                time.sleep(timeperrequest)
             elif added and lossed:
                 webhook = DiscordWebhook(url=Webhook, content=f"**{name}** has changed their outfit!: \n**Wearing**: = {active} \n**Added**: {added} \n**Removed**: {lossed}")
+                webhook.add_embed(embed)
                 response = webhook.execute()
                 print("none is null")
+                oldwear = active
+                time.sleep(timeperrequest)
             elif lossed:
                 webhook = DiscordWebhook(url=Webhook, content=f"**{name}** has changed their outfit!: \n**Wearing**: = {active} \n**Nothing Added** \n**Removed**: {lossed}")
+                webhook.add_embed(embed)
                 response = webhook.execute()
                 print("added is null")
+                oldwear = active
+                time.sleep(timeperrequest)
             elif added:
                 webhook = DiscordWebhook(url=Webhook, content=f"**{name}** has changed their outfit!: \n**Wearing**: = {active} \n**Added**: {added} \n**Nothing Removed**")
+                webhook.add_embed(embed)
                 response = webhook.execute()
                 print("loss is null")
+                oldwear = active
+                time.sleep(timeperrequest)
             checkwearing = 0
-            time.sleep(timeperrequest)
         else:
             print("outfit is the same")
             time.sleep(timeperrequest)
